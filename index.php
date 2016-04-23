5bc96acdf79e29d9e8c5496048bc52f061927935
langhuaili <757501172@qq.com>
2016-04-23 19:41:17 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+a way to use curl skills to get the passage of a webiste <?php
+
+function show_side($tgurl){ $wwwtext=get_url_content($tgurl); $str=$wwwtext;
+
+//取head标签内容 preg_match("/(.*)<\/title>/si",$str,$match); $headstr = trim($match[1]); //return $headstr; echo '<li><a href="'.$tgurl.'">'.$headstr.'</a></li>'; }</p> <p>function get_url_content($url){ if(function_exists("curl_init")){ $ch = curl_init(); $timeout = 60; curl_setopt($ch, CURLOPT_URL, $url); curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // curl_setopt($ch, CURLOPT_NOBODY, 1); curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout); $str = curl_exec($ch); curl_close($ch); }else{ if(ini_get('allow_url_fopen')){$str = file_get_contents($url);} } return $str; }</p> <p>//配置URL $url = array('<a href="http://www.ifeng.com/','http://www.baidu.com/','http://www.ifeng.com/','http://www.baidu.com/','http://www.ifeng.com/','http://www.baidu.com/">http://www.ifeng.com/','http://www.baidu.com/','http://www.ifeng.com/','http://www.baidu.com/','http://www.ifeng.com/','http://www.baidu.com/</a>' );</p> <p>foreach ($url as $value) { show_side($value);}</p>
